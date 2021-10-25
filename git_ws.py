37b4f9f0f7a670e701ac85763fc586815bc83aa4
Enster88 <62439107+Enster88@users.noreply.github.com>
2021-10-25 14:15:47 +0200

    Merge

--- git_ws.py
+++ git_ws.py
@@ -19,11 +19,7 @@
 #Git 3
 #Git 1
 #Git 5
-<<<<<<< HEAD
 Push it Push it
-=======
-
-Git 1
 Git 2
 Git 876
 Git 23\
@@ -31,4 +27,3 @@ Git 5663
 Git 342
 Git 454
 Git 21323
->>>>>>> 196c1b590aa0325e8b11897eda0d94b23bdfcac3
